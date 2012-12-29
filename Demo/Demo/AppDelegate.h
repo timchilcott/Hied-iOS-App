766e0c3b55a3e898e4ca55889f57110613cad508
Kevin <kevinpk@gmail.com>
2012-12-28 19:59:02 -0600

    Added Demo Xcode project
    Take a look at the screen capture to see where to manipulate the icons,
    images, views, etc.

---
+++ Demo/Demo/AppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  AppDelegate.h
+//  Demo
+//
+//  Created by Kevin Kelly on 12/28/12.
+//  Copyright (c) 2012 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class ViewController;
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) ViewController *viewController;
+
+@end
---
+++ Demo/Demo/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  Demo
+//
+//  Created by Kevin Kelly on 12/28/12.
+//  Copyright (c) 2012 KK. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
